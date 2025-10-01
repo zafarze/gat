3558a60030bccfd949a5c79f2f4ad100556a887b
Zafar <zafarze@yandex.ru>
2025-10-02 03:04:36 +0500

    Remove PostgreSQL-specific DB option

--- config/settings.py
+++ config/settings.py
@@ -91,16 +91,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.mysql', # Это уже правильно
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5433'),
-        # Добавляем кодировку для корректной работы с PostgreSQL
-        'OPTIONS': {
-            'client_encoding': 'UTF8'
-        },
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
